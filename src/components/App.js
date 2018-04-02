e2d5f2acdccdae300864795641625ece12f10efa
Ben Slabbert <ben@hex.co.za>
2018-04-02 12:00:38 +0200

    formatting

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="logo"/>
+                    <h1 className="app-title">Welcome to React</h1>
+                </header>
+                <p className="app-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
